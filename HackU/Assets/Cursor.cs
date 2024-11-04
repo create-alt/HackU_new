c0e7fd933dd3fe74050038534f169836385fe7f3
create-alt <toshikakimika@gmail.com>
2024-11-04 17:05:44 +0900

    can mouth playing

--- HackU/Assets/Cursor.cs
+++ HackU/Assets/Cursor.cs
@@ -9,12 +9,14 @@ using UnityEngine;
 public class Cursor : MonoBehaviour
 {
 
+    public bool use_micon = true; //マイコンを使用するならtrue、使用しないならfalse
+
     [SerializeField] GameObject goal_serial;
     Vector3 target_serial;
 
     BluetoothReceiver target_script;
-    private float X=0f, Y=0f, Z=0f, gyro=0f;
-    private int is_fire=0;
+    private float X = 0f, Y = 0f, Z = 0f, gyro = 0f;
+    private int is_fire = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -30,40 +32,54 @@ public class Cursor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        try
+        if (use_micon)
         {
-            target_script = GameObject.Find("Reciever").GetComponent<BluetoothReceiver>();
-            X -= target_script.gyroZ;
-            Y -= target_script.gyroX;
-            Z = target_script.yaw;
+            try
+            {
+                target_script = GameObject.Find("Reciever").GetComponent<BluetoothReceiver>();
+                X -= target_script.gyroZ;
+                Y -= target_script.gyroX;
+                Z = target_script.yaw;
 
-            //Debug.Log(target_script.yaw);
+                //Debug.Log(target_script.yaw);
 
-            is_fire = target_script.is_fire;
+                is_fire = target_script.is_fire;
 
-            // X, Yの範囲を制限
-            // X = Mathf.Clamp((float)X, -30f, 30f);
-            X = Mathf.Clamp((float)X, -40f, 40f);
+                // X, Yの範囲を制限
+                // X = Mathf.Clamp((float)X, -30f, 30f);
+                X = Mathf.Clamp((float)X, -40f, 40f);
 
-            //Y = (float)Math.Round(Y, 2, MidpointRounding.AwayFromZero);
-            Y = Mathf.Clamp((float)Y, -23f, 23f);
+                //Y = (float)Math.Round(Y, 2, MidpointRounding.AwayFromZero);
+                Y = Mathf.Clamp((float)Y, -23f, 23f);
 
-            // 現在のオブジェクトの位置
-            // Vector3 currentPos = transform.position;
+                // 現在のオブジェクトの位置
+                // Vector3 currentPos = transform.position;
 
-            // スクリーン座標をワールド座標に変換
-            //Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(0f, (float)Z, 30f));
+                // スクリーン座標をワールド座標に変換
+                //Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(0f, (float)Z, 30f));
 
-            transform.position = new Vector3(X,(float)Y, 5f);
+                transform.position = new Vector3(X, (float)Y, 5f);
 
-            // 補間して滑らかに移動
-            // transform.position = Vector3.Lerp(currentPos, targetPos, 0.1f); // 第三引数は補間の割合
+                // 補間して滑らかに移動
+                // transform.position = Vector3.Lerp(currentPos, targetPos, 0.1f); // 第三引数は補間の割合
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log("Error reading serial port");
+            }
         }
-        catch (System.Exception e)
+        else
         {
-            Debug.Log("Error reading serial port");
+            // マウスの座標を取得し、それに基づいてオブジェクトの位置を更新する
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 25f; // カメラからの距離を設定
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            // オブジェクトの位置をマウスのワールド座標に設定
+            transform.position = worldPosition;
         }
+
+
 
     }
 }
--- HackU/Assets/Gun.cs
+++ HackU/Assets/Gun.cs
@@ -6,23 +6,29 @@ public class Gun : MonoBehaviour
 {
     public AudioSource hitSound;
 
+
+
     private bool isShooting = false; // コルーチンが実行中かどうかのフラグ
     private bool checker = false;
 
+
     BluetoothReceiver target_script;
+    private bool use_micon;
 
     // Start is called before the first frame update
     void Start()
     {
     }
 
-    private IEnumerator Hoge(){
-        
+    private IEnumerator Hoge()
+    {
+
         isShooting = true;
 
-        
-        while(isShooting){
-        //while (Input.GetMouseButton(0)){    
+
+        while (isShooting)
+        {
+            //while (Input.GetMouseButton(0)){    
             //balletプレハブをGameObject型で取得
             GameObject obj = (GameObject)Resources.Load("ballet");
 
@@ -36,23 +42,37 @@ public class Gun : MonoBehaviour
         }
 
         checker = false;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         target_script = GameObject.Find("Reciever").GetComponent<BluetoothReceiver>();
-
-        if (target_script.is_fire == 1)
+        use_micon = GameObject.Find("Cursor").GetComponent<Cursor>().use_micon;
+        if (use_micon)
         {
-            checker = true;
-            Debug.Log("checker is true");
-            hitSound.Play();
+            if (target_script.is_fire == 1)
+            {
+                checker = true;
+                Debug.Log("checker is true");
+                hitSound.Play();
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                checker = true;
+                Debug.Log("checker is true");
+                hitSound.Play();
+            }
         }
 
 
-        if (checker && !(isShooting)){
+
+        if (checker && !(isShooting))
+        {
             //コルーチンの実行
             StartCoroutine(Hoge());
         }
